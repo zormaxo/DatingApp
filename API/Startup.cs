b0af74e9898dd2fe769bff4be8df9b235f426702
zormax <zormax052@gmail.com>
2022-02-21 09:10:09 +0300

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,7 +43,7 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
-      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
