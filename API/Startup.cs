bfaf22cd7c0d3a0eb80110a0b816444246afb3b9
zormax <zormax052@gmail.com>
2022-03-20 01:05:28 +0300

    end of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,4 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -16,14 +15,10 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
-
+      services.AddApplicationServices(_config);
       services.AddControllers();
       services.AddCors();
-
+      services.AddIdentityServices(_config);
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -43,8 +38,10 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
       app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
