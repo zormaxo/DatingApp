c6e8d83179b7d2aa1dce5c9ec9f5e6d00b90f16c
zormax <zormax052@gmail.com>
2022-06-14 07:06:25 +0300

    end of lesson 91

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,26 +6,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  public class UsersController : BaseApiController
-  {
-    private readonly DataContext _context;
-    public UsersController(DataContext context)
-    {
-      _context = context;
-    }
-
-    [AllowAnonymous]
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-      return await _context.Users.ToListAsync();
-    }
-
     [Authorize]
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    public class UsersController : BaseApiController
     {
-      return await _context.Users.FindAsync(id);
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
     }
-  }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,35 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
             {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
